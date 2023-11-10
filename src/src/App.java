47c1123e32eb68043d867daf9a64530457410b89
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-11-09 18:12:56 -0700

    Renamed some stuff

--- src/src/App.java
+++ src/src/App.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package adtsandxmlparser;
+package src;
 
 /**
  *
  * @author garry
  */
-public class ADTsAndXMLParser {
+public class App {
 
     /**
      * @param args the command line arguments
